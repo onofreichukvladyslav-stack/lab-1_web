b5b9a6d03c88f5317e1eae36e7d086ad45873da0
onofreichukvladyslav-stack <onofreichuk.vladyslav@chnu.edu.ua>
2026-03-13 01:18:02 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,22 @@
+let btn = document.getElementById('btnCheck');
+let result = document.getElementById('result');
+
+btn.addEventListener('click', function(){
+    const numbers = document.getElementById('numbers').value;
+    const digits = document.getElementById('digits').value;
+
+    if (!numbers || !digits){
+        result.innerHTML = "Заповніть поля!";
+        return;
+    }
+
+    let count = 0;
+
+    for (let char of numbers){
+        if (char === digits){
+            count++;
+        }
+    }
+
+    result.innerHTML = "Число " + digits + " зустрічається " + count + " раз(ів).";
+});
